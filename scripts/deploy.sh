63a33be020fc59dda85b640216a4f633f7e35c57
dpinones <pinonesdamian@gmail.com>
2024-02-19 00:42:47 -0300

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,22 +7,22 @@ GREEN='\e[32m'
 RESET='\e[0m'
 
 # Declare all contracts
-echo -e "$GREEN\n==> Declare HelloStarknet$RESET"
+echo -e "$GREEN\n==> Declare Pathfinder$RESET"
 DECLARED_CLASSHASH=$(starkli declare --watch \
     --account $ACCOUNT_SRC \
     --rpc $RPC_URL \
     --keystore $KEYSTORE_SRC \
     --keystore-password $KEYSTORE_PASSWORD \
-    ./target/dev/cairo_template_HelloStarknet.contract_class.json)
+    ./target/dev/pathfinding_Pathfinder.contract_class.json)
 
 echo -e "$GREEN\nDeclared Classhash: $DECLARED_CLASSHASH$RESET"
 
-echo -e "$GREEN\n==> Deploy HelloStarknet$RESET"
-CONTRACT_ADDRESS=$(starkli deploy --watch \
-    --account $ACCOUNT_SRC \
-    --rpc $RPC_URL \
-    --keystore $KEYSTORE_SRC \
-    --keystore-password $KEYSTORE_PASSWORD \
-    $DECLARED_CLASSHASH)
+# echo -e "$GREEN\n==> Deploy Pathfinder$RESET"
+# CONTRACT_ADDRESS=$(starkli deploy --watch \
+#     --account $ACCOUNT_SRC \
+#     --rpc $RPC_URL \
+#     --keystore $KEYSTORE_SRC \
+#     --keystore-password $KEYSTORE_PASSWORD \
+#     $DECLARED_CLASSHASH)
 
-echo -e "$GREEN$\nDeployed contract address: $CONTRACT_ADDRESS$RESET"
+# echo -e "$GREEN$\nDeployed contract address: $CONTRACT_ADDRESS$RESET"
